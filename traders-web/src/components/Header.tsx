cf584f5d02f79c2e4c76b048572da92a3471ecba
Maneesh Singh <maneesh54321@gmail.com>
2019-04-10 03:41:38 +0530

    Fix Client details page

--- traders-web/src/components/Header.tsx
+++ traders-web/src/components/Header.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import SearchBar from "./SearchBar";
+import {Route, Switch} from "react-router";
 
 export default class Header extends React.Component {
     render(){
@@ -35,11 +37,10 @@ export default class Header extends React.Component {
                             <a className="nav-link disabled" href="#">Disabled</a>
                         </li>
                     </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search"
-                               aria-label="Search"/>
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
+                    <Switch>
+                        <Route path="/home" render={ () => null } />
+                        <Route render={ () => <SearchBar/> } />
+                    </Switch>
                 </div>
             </nav>
         );
--- traders-web/src/containers/ClientDetails.tsx
+++ traders-web/src/containers/ClientDetails.tsx
@@ -4,19 +4,19 @@ export default class ClientDetails extends React.Component {
     render() {
         return (
             <div className="container">
-                <div className="card border-0">
-                    <div className="card-header bg-success">
-                        <h4 className="card-title">
+                <div className="card form-card my-3">
+                    <div className="card-header">
+                        <h4 style={{ marginBottom: '0' }}>
                             Client Name
                         </h4>
                     </div>
-                    <div className="card-body p-5">
+                    <div className="card-body">
                         <div className="row">
                             <div className="col-md-12 form-section-header">
                                 Basic Details
                             </div>
                         </div>
-                        <div className="form-row border-bottom pb-3">
+                        <div className="form-row pb-3 px-3">
                             <div className="col-lg-6">
                                 <div className="row">
                                     <label htmlFor="bdrId" className="col-sm-5 col-form-label">
@@ -42,7 +42,7 @@ export default class ClientDetails extends React.Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="form-row">
+                        <div className="form-row px-3">
                             <div className="col-lg-6">
                                 <div className="row">
                                     <label htmlFor="registeredAddress" className="col-sm-5 col-form-label">
@@ -70,78 +70,82 @@ export default class ClientDetails extends React.Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="row">
-                            <div className="col-md-12 form-section-header">
-                                <span>Billing Info</span>
+                        <div>
+                            <div className="row mt-3">
+                                <div className="col-md-12 form-section-header">
+                                    <span>Billing Info</span>
+                                </div>
                             </div>
+                            <form action="" className="px-3">
+                                <div className="form-row pb-3">
+                                    <div className="form-group col-md-3">
+                                        <label htmlFor="accountType">Account Type</label>
+                                        <select name="accountType" id="accountType" className="form-control">
+                                            <option value="">Lorem</option>
+                                            <option value="">Ipsum</option>
+                                            <option value="">Finibus</option>
+                                            <option value="">Bonorum</option>
+                                        </select>
+                                    </div>
+                                    <div className="form-group col-md-3 offset-md-1">
+                                        <label htmlFor="rechargeType">Recharge Type</label>
+                                        <select name="rechargeType" id="rechargeType" className="form-control">
+                                            <option value="">Lorem Ipsum</option>
+                                            <option value="">Aldus PageMaker</option>
+                                            <option value="">Letraset sheets</option>
+                                            <option value="">specimen book</option>
+                                        </select>
+                                    </div>
+                                    <div className="form-group col-md-4 offset-md-1">
+                                        <label htmlFor="rechargeReason">Recharge Reason</label>
+                                        <select name="rechargeReason" id="rechargeReason" className="form-control">
+                                            <option value="">Contrary to popular belief, Lorem Ipsum </option>
+                                            <option value="">Latin literature from 45 BC, making it over 2000</option>
+                                            <option value="">the majority have suffered alteration in some form</option>
+                                            <option value="">All the Lorem Ipsum generators on the Internet</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="form-group pb-3">
+                                    <label htmlFor="otherReason">Other Reason</label>
+                                    <textarea className="form-control" placeholder="Enter the other reason here" id="otherReason"/>
+                                </div>
+                                <div className="form-row pb-3">
+                                    <div className="form-group col-md-3">
+                                        <label htmlFor="euVat">EU Vat number</label>
+                                        <input type="text" name="euVat" id="euVat" className="form-control"/>
+                                    </div>
+                                    <div className="form-group col-md-3 offset-md-1">
+                                        <label htmlFor="ukVat">UK vat number</label>
+                                        <input type="text" name="ukVat" id="ukVat" className="form-control"/>
+                                    </div>
+                                    <div className="form-group col-md-4 offset-md-1">
+                                        <label htmlFor="directInvoicingEmail">Direct invoicing email</label>
+                                        <input type="email" name="directInvoicingEmail" id="directInvoicingEmail"
+                                               className="form-control"/>
+                                    </div>
+                                </div>
+                                <div className="form-row">
+                                    <div className="form-group col-md-3">
+                                        <label htmlFor="vatPercent">VAT (%)</label>
+                                        <input type="text" name="vatPercent" id="vatPercent" className="form-control"/>
+                                    </div>
+                                    <div className="form-group col-md-3 offset-md-1">
+                                        <label htmlFor="eTradingCost">E-Trading Cost</label>
+                                        <input type="number" name="eTradingCost" id="eTradingCost"
+                                               className="form-control"/>
+                                    </div>
+                                    <div className="form-group col-md-4 offset-md-1">
+                                        <label htmlFor="billingRegion">E-Trading Cost</label>
+                                        <input type="text" name="billingRegion" id="billingRegion"
+                                               className="form-control"/>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        <form action="">
-                            <div className="form-row pb-3">
-                                <div className="form-group col-md-3">
-                                    <label htmlFor="accountType">Account Type</label>
-                                    <select name="accountType" id="accountType" className="form-control">
-                                        <option value="">Lorem</option>
-                                        <option value="">Ipsum</option>
-                                        <option value="">Finibus</option>
-                                        <option value="">Bonorum</option>
-                                    </select>
-                                </div>
-                                <div className="form-group col-md-3 offset-md-1">
-                                    <label htmlFor="rechargeType">Recharge Type</label>
-                                    <select name="rechargeType" id="rechargeType" className="form-control">
-                                        <option value="">Lorem Ipsum</option>
-                                        <option value="">Aldus PageMaker</option>
-                                        <option value="">Letraset sheets</option>
-                                        <option value="">specimen book</option>
-                                    </select>
-                                </div>
-                                <div className="form-group col-md-4 offset-md-1">
-                                    <label htmlFor="rechargeReason">Recharge Reason</label>
-                                    <select name="rechargeReason" id="rechargeReason" className="form-control">
-                                        <option value="">Contrary to popular belief, Lorem Ipsum </option>
-                                        <option value="">Latin literature from 45 BC, making it over 2000</option>
-                                        <option value="">the majority have suffered alteration in some form</option>
-                                        <option value="">All the Lorem Ipsum generators on the Internet</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="form-group pb-3">
-                                <label htmlFor="otherReason">Other Reason</label>
-                                <textarea className="form-control" placeholder="Enter the other reason here" id="otherReason"/>
-                            </div>
-                            <div className="form-row pb-3">
-                                <div className="form-group col-md-3">
-                                    <label htmlFor="euVat">EU Vat number</label>
-                                    <input type="text" name="euVat" id="euVat" className="form-control"/>
-                                </div>
-                                <div className="form-group col-md-3 offset-md-1">
-                                    <label htmlFor="ukVat">UK vat number</label>
-                                    <input type="text" name="ukVat" id="ukVat" className="form-control"/>
-                                </div>
-                                <div className="form-group col-md-4 offset-md-1">
-                                    <label htmlFor="directInvoicingEmail">Direct invoicing email</label>
-                                    <input type="email" name="directInvoicingEmail" id="directInvoicingEmail"
-                                           className="form-control"/>
-                                </div>
-                            </div>
-                            <div className="form-row">
-                                <div className="form-group col-md-3">
-                                    <label htmlFor="vatPercent">VAT (%)</label>
-                                    <input type="text" name="vatPercent" id="vatPercent" className="form-control"/>
-                                </div>
-                                <div className="form-group col-md-3 offset-md-1">
-                                    <label htmlFor="eTradingCost">E-Trading Cost</label>
-                                    <input type="number" name="eTradingCost" id="eTradingCost"
-                                           className="form-control"/>
-                                </div>
-                                <div className="form-group col-md-4 offset-md-1">
-                                    <label htmlFor="billingRegion">E-Trading Cost</label>
-                                    <input type="text" name="billingRegion" id="billingRegion"
-                                           className="form-control"/>
-                                </div>
-                            </div>
-                            <button className="btn btn-primary">Update</button>
-                        </form>
+                    </div>
+                    <div className="card-footer">
+                        <button className="btn btn-danger px-5">Update</button>
                     </div>
                 </div>
             </div>
