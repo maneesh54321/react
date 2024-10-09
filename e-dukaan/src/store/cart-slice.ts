6c5f2a3ab97a368f23f5d29c08a802d997fe8be7
maneesh <maneesh54321@gmail.com>
2024-10-09 10:47:48 +0530

    [e-dukaan] Save cart state to localstorage

--- e-dukaan/src/store/cart-slice.ts
+++ e-dukaan/src/store/cart-slice.ts
@@ -1,46 +1,65 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 import { CartItem } from "../components/cart/CartItemCard";
+import Cart from "../components/cart/Cart";
 
-const initialState: {
+interface Cart {
   items: CartItem[];
-} = {
-  items: [],
-};
+}
+
+function loadCartFromLocalStorage(): Cart {
+  const cart = localStorage.getItem("cart");
+
+  if (cart) {
+    return JSON.parse(cart);
+  }
+  return {
+    items: [],
+  };
+}
+
+const initialState: Cart = loadCartFromLocalStorage();
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: initialState,
   reducers: {
-    addItem(state, action) {
-      const product = action.payload;
-      const idx = state.items.findIndex(
-        (item) => item.product.id === product.id
-      );
-      if (idx >= 0) {
-        state.items[idx].quantity += 1;
-      } else {
-        state.items.push({
-          product,
-          quantity: 1,
-        });
-      }
-    },
-    removeItem(state, action) {
-      const product = action.payload;
-      const idx = state.items.findIndex(
-        (item) => item.product.id === product.id
-      );
-      if (idx >= 0) {
-        if (state.items[idx].quantity > 1) {
-          state.items[idx].quantity -= 1;
+    addItem: (state, action) =>
+      saveCartToLocalStorage(() => {
+        const product = action.payload;
+        const idx = state.items.findIndex(
+          (item) => item.product.id === product.id
+        );
+        if (idx >= 0) {
+          state.items[idx].quantity += 1;
         } else {
-          state.items.splice(idx, 1);
+          state.items.push({
+            product,
+            quantity: 1,
+          });
         }
-      }
-    },
+      }, state),
+    removeItem: (state, action) =>
+      saveCartToLocalStorage(() => {
+        const product = action.payload;
+        const idx = state.items.findIndex(
+          (item) => item.product.id === product.id
+        );
+        if (idx >= 0) {
+          if (state.items[idx].quantity > 1) {
+            state.items[idx].quantity -= 1;
+          } else {
+            state.items.splice(idx, 1);
+          }
+        }
+      }, state),
   },
 });
+
+function saveCartToLocalStorage(updateCart: () => void, cart: Cart) {
+  updateCart();
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export default cartSlice;
 
